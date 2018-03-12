b54472c3d5f80808202b858f9c7be857a297f6e7
Fahri MacBook <fahri-can@web.de>
2018-03-13 00:16:02 +0100

    Restore start code.

--- exercise2/src/main/java/at/technikumwien/birthdaynotifier/ui/main/MainActivity.java
+++ exercise2/src/main/java/at/technikumwien/birthdaynotifier/ui/main/MainActivity.java
@@ -17,7 +17,11 @@ import at.technikumwien.birthdaynotifier.ui.main.recyclerview.ContactAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView title;
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
+    private TextView emptyText;
+
+    private ContactAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content view and find our views
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
+        fab = findViewById(R.id.fab);
+        emptyText = findViewById(R.id.no_contacts);
 
-        title = findViewById(R.id.title);
-        title.setText("Test Test ");
+        // Create an adapter for the RecyclerView
+        adapter = new ContactAdapter();
+
+        // If the recycler view does not change in size, this
+        // enables some optimizations
+        recyclerView.setHasFixedSize(true);
+        // Set a LinearLayoutManager, which lays the items out one after another
+        // (like a LinearLayout)
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Set our contacts adapter on the RecyclerView
+        recyclerView.setAdapter(adapter);
+
+        // Show a snackbar when the floating action button is clicked
+        // and add some data to show
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(fab, "Kontakte geladen", Snackbar.LENGTH_LONG).show();
+                onContactsLoaded(Arrays.asList(
+                        "Max Mustermann",
+                        "Maria Musterfrau",
+                        "Stefan Müller",
+                        "Theresa Huber",
+                        "Manuel Mustermann",
+                        "Stefanie Musterfrau",
+                        "Thomas Müller",
+                        "Sarah Huber"
+                ));
+            }
+        });
     }
 
+    // Is called, when contacts are loaded. Here we set the visibility
+    // of the empty list info text and update the data in our adapter
+    private void onContactsLoaded(List<String> contactList) {
+        emptyText.setVisibility(contactList.isEmpty() ? View.VISIBLE : View.GONE);
+        adapter.setContactList(contactList);
+    }
 }
