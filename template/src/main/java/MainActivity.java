d1f5b386c22ab91d795396a06547a5f6642a4a9b
Fahri MacBook <fahri-can@web.de>
2018-03-31 17:18:48 +0200

    Add main UI.

---
+++ template/src/main/java/MainActivity.java
@@ -0,0 +1,22 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import at.technikumwien.birthdaynotifier.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
+    }
+}//end of class MainActivity
