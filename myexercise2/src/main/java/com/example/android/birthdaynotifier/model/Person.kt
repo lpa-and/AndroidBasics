62c9f132b1da64809e580a43cdc0d00e7038e55d
Fahri MacBook <fahri-can@web.de>
2018-04-22 19:51:45 +0200

    Create Person class and the person cards.

---
+++ myexercise2/src/main/java/com/example/android/birthdaynotifier/model/Person.kt
@@ -0,0 +1,8 @@
+package com.example.android.birthdaynotifier.model
+
+class Person {
+
+    var personIcon: Int? = null
+    var personName: String = "Max Mustermann"
+    var personBirthday: String = "01.01.1999"
+}
---
+++ myexercise2/src/main/java/com/example/android/birthdaynotifier/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.birthdaynotifier.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.android.birthdaynotifier.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
