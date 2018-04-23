603b8f348469c419f8b32b4924a606862021a296
Fahri MacBook <fahri-can@web.de>
2018-04-23 13:47:09 +0200

    Refatcor and add comments.

--- myexercise2/src/main/java/com/example/android/birthdaynotifier/model/Person.kt
+++ myexercise2/src/main/java/com/example/android/birthdaynotifier/model/Person.kt
@@ -2,15 +2,6 @@ package com.example.android.birthdaynotifier.model
 
 import com.example.android.birthdaynotifier.R
 
-class Person {
+class Person(var personIcon: Int, var personName: String, var personBirthday: String) {
 
-    var personIcon: Int = 0
-    var personName: String = "Max Mustermann"
-    var personBirthday: String = "01.01.1999"
-
-    constructor(personIcon: Int, personName: String, personBirthday: String) {
-        this.personIcon = personIcon
-        this.personName = personName
-        this.personBirthday = personBirthday
-    }
 }
--- myexercise2/src/main/java/com/example/android/birthdaynotifier/ui/MainActivity.kt
+++ myexercise2/src/main/java/com/example/android/birthdaynotifier/ui/MainActivity.kt
@@ -2,19 +2,15 @@ package com.example.android.birthdaynotifier.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.NonNull
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
-
 import com.example.android.birthdaynotifier.R
 import com.example.android.birthdaynotifier.adapter.PersonAdapter
 import com.example.android.birthdaynotifier.model.Person
-import java.sql.Array
-import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     var recyclerView: RecyclerView? = null
     var adapter: PersonAdapter? = null
     var emptyText: TextView? = null
-    val personList: ArrayList<Person>? = ArrayList()
+    val personList: ArrayList<Person> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,24 +27,26 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         val fab: FloatingActionButton = findViewById(R.id.fab)
         emptyText = findViewById(R.id.empty_text)
-        adapter = PersonAdapter(this, personList!!)
+        adapter = PersonAdapter(this, personList)
 
+        // If the recycler view does not change in size, this enables some optimizations
         recyclerView?.setHasFixedSize(true)
+        // Set a LinearLayoutManager, which lays the items out one after another(like LinearLayout)
         recyclerView?.layoutManager = LinearLayoutManager(this)
         recyclerView?.adapter = this.adapter
 
+        // Show a snackbar when the floating action button is clicked and add some data to show
         fab.setOnClickListener {
 
             Snackbar.make(fab, "Kontakte geladen", Snackbar.LENGTH_LONG).show()
-            onContactsLoaded()
+            loadPersonDataInList()
         }
     }
 
 
-    private fun onContactsLoaded() {
+    private fun loadPersonDataInList() {
 
-
-        var contactList: ArrayList<Person> = ArrayList()
+        val contactList: ArrayList<Person> = ArrayList()
         contactList.add(Person(R.id.personIcon, "Max Mustermann", "01.09.1999"))
         contactList.add(Person(R.id.personIcon, "Maria Musterfrau", "01.09.1999"))
         contactList.add(Person(R.id.personIcon, "Stefan Müller", "01.09.1999"))
@@ -63,8 +61,10 @@ class MainActivity : AppCompatActivity() {
         contactList.add(Person(R.id.personIcon, "Manuel Bauer", "01.09.1999"))
         contactList.add(Person(R.id.personIcon, "Stefanie Müller", "01.09.1999"))
 
+        personList.addAll(contactList)
+
         emptyText?.visibility = View.GONE
 
-        adapter?.setPersonList(contactList)
+        adapter?.setPersonList(personList)
     }
 }
